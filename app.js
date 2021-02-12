1c6c4df271d18aaa6291b09c2084f8ba921c1876
bayanaldaoud <bayan.and95@gmail.com>
2021-02-11 17:24:20 -0800

    prob 3 done

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) {
-  //eslint-disable-line
+  let numSum = a + b;
+  return [numSum, 'The sum of ' + a + ' and ' + b + ' is ' + numSum + '.'];
 }
-console.log(sum(4,7));
-
-
+console.log(sum(4, 7));
 // Here is the test for sum(); uncomment it to run it
- testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -46,22 +45,31 @@ testMultiply(5,9);
 
 /////////////////////////////////////
 /* Problem 3
-Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
+Write a function called sumAndMultiply() that takes in three numbers as separate arguments
+ and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,
+   and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, 
+and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to
+ do this. However, you may continue to use the + operator for string concatenation.
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
+Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for
+ the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
 function sumAndMultiply(a, b, c) {
-  //eslint-disable-line
+  let mySum  = a + b + c; 
+  let myMultiply = a * b * c ;
+  return [mySum, myMultiply, a + ' and ' + b + ' and ' + c + ' sum to ' + mySum + '.' , 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + myMultiply + '.'];
+  
 }
-
+console.log(sumAndMultiply(4,7,5));
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
